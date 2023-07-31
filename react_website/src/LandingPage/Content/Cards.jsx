2e317852f5cf21cff8b42af3311a6f0a34500eb0
Godslove Udo <80900180+G0d5l0v3@users.noreply.github.com>
2023-07-31 09:07:36 +0100

    update

--- react_website/src/LandingPage/Content/Cards.jsx
+++ react_website/src/LandingPage/Content/Cards.jsx
@@ -4,32 +4,33 @@ import { Card } from 'antd';
 const Cards = () => {
   const cardData = [
     {
-      title: 'Card 1',
-      content: 'This is the content of Card 1.',
+      title: 'App Login Page',
+      content: '',
     },
     {
-      title: 'Card 2',
-      content: 'This is the content of Card 2.',
+      title: 'Products Card',
+      content: '',
     },
     {
-      title: 'Card 3',
+      title: 'Age Calculator',
       content: '',
     },
   ];
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+    <div className = "flex justify-around" >
       {cardData.map((item, index) => (
         <Card
           key={index}
           title={item.title}
-          style={{
+          /*style={{
             width: 300,
             height: 400,
             borderRadius: 8,
             boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
             marginBottom: 50
-          }}
+          }}*/
+          className = "w-[300px] h-[400px] mb-[50px] shadow-md"
         >
           <p>{item.content}</p>
         </Card>
