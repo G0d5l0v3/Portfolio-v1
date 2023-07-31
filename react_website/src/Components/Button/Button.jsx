06bd6f593ca54bedb4ccf2c91f84853dbc18bc02
Godslove Udo <80900180+G0d5l0v3@users.noreply.github.com>
2023-07-31 20:51:15 +0100

    update

--- react_website/src/Components/Button/Button.jsx
+++ react_website/src/Components/Button/Button.jsx
@@ -1,24 +1,37 @@
-import React from 'react';
+import React from "react";
 import getStart from "../../Assets/Images/getstart.svg";
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
 const Button = ({ type, text, onClick }) => {
-    switch (type.toLowerCase()) {
-        case "black":
-            return <button className="flex gap-[1rem] items-center bg-[#000000] px-[1.2rem] py-[0.5rem] font-[archivo-medium] text-[#FFFFFF] text-[1rem] rounded-[2rem]" onClick={onClick}>{text}  <img src={getStart} alt="" /></button>
-            break;
-        case "white":
-            return (<NavLink 
-            to="https://drive.google.com/file/d/1s2ACCbhbbE9TAPA5GcxGt5jhzERBWEBQ/view?usp=sharing"
-            >
-                <button className="flex items-center bg-[#FFFFFF] px-[1.2rem] py-[0.5rem] font-[archivo-regular] text-[#000000] text-[1rem] rounded-[2rem] outline-none outline-1 outline-[#dedede]" onClick={onClick}> {text} <img src={getStart} alt="" /> </button>
-
-            </NavLink>)
-            break;
-        case "default":
-            break;
-    }
-
-}
+  switch (type.toLowerCase()) {
+    case "black":
+      return (
+        <NavLink to="">
+          <button
+            className="flex gap-[1rem] items-center bg-[#000000] px-[1.2rem] py-[0.5rem] font-[archivo-medium] text-[#FFFFFF] text-[1rem] rounded-[2rem]"
+            onClick={onClick}
+          >
+            {text} <img src={getStart} alt="" />
+          </button>
+        </NavLink>
+      );
+      break;
+    case "white":
+      return (
+        <NavLink to="https://drive.google.com/file/d/1s2ACCbhbbE9TAPA5GcxGt5jhzERBWEBQ/view?usp=sharing">
+          <button
+            className="flex items-center bg-[#FFFFFF] px-[1.2rem] py-[0.5rem] font-[archivo-regular] text-[#000000] text-[1rem] rounded-[2rem] outline-none outline-1 outline-[#dedede]"
+            onClick={onClick}
+          >
+            {" "}
+            {text} <img src={getStart} alt="" />{" "}
+          </button>
+        </NavLink>
+      );
+      break;
+    case "default":
+      break;
+  }
+};
 
 export default Button;
--- react_website/src/LandingPage/Header/Header.jsx
+++ react_website/src/LandingPage/Header/Header.jsx
@@ -113,7 +113,7 @@ const Header = () => {
             <Button
               type="white"
               text="Preview CV"
-              className="w-full "
+              className="w-full sm:w-auto"
             ></Button>
           </div>
         </main>
