76d873bb64061c2c93aa0d8118e38d2ffcc2066d
Godslove Udo <80900180+G0d5l0v3@users.noreply.github.com>
2023-07-31 18:56:29 +0100

    Update Button.jsx

--- react_website/src/Components/Button/Button.jsx
+++ react_website/src/Components/Button/Button.jsx
@@ -7,12 +7,12 @@ const Button = ({ type, text, onClick }) => {
             return <button className="flex gap-[1rem] items-center bg-[#000000] px-[1.2rem] py-[0.5rem] font-[archivo-medium] text-[#FFFFFF] text-[1rem] rounded-[2rem]" onClick={onClick}>{text}  <img src={getStart} alt="" /></button>
             break;
         case "white":
-            return <NavLink 
+            return (<NavLink 
             to="https://drive.google.com/drive/u/0/my-drive"
             >
                 <button className="flex items-center bg-[#FFFFFF] px-[1.2rem] py-[0.5rem] font-[archivo-regular] text-[#000000] text-[1rem] rounded-[2rem] outline-none outline-1 outline-[#dedede]" onClick={onClick}> {text}  </button>
 
-            </NavLink>
+            </NavLink>)
             break;
         case "default":
             break;
