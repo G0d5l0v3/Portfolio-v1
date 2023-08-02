52d3b077415dc3220d1f3fe1deb3786317f41c2f
Godslove Udo <80900180+G0d5l0v3@users.noreply.github.com>
2023-08-02 15:21:39 +0100

    Update Cards.jsx

--- react_website/src/LandingPage/Content/Cards.jsx
+++ react_website/src/LandingPage/Content/Cards.jsx
@@ -1,25 +1,36 @@
-import React from 'react';
-import { Card } from 'antd';
-
+import React from "react";
+import { Card } from "antd";
 
 const Cards = () => {
   const cardData = [
     {
-      title: 'App Login Page',
-      content: '',
+      title: "App Login Page",
+      content: "",
     },
     {
-      title: 'FakeStore Api Products Card',
-      content: '',
+      title: "FakeStore Api Products Card",
+      content: "",
     },
     {
-      title: 'Age Calculator',
-      content: '',
+      title: "Age Calculator",
+      content: "",
+    },
+    {
+      title: "",
+      content: "",
+    },
+    {
+      title: "",
+      content: "",
+    },
+    {
+      title: "",
+      content: "",
     },
   ];
 
   return (
-    <div className = "flex items-center flex-wrap justify-around" >
+    <div className="flex items-center flex-wrap justify-around">
       {cardData.map((item, index) => (
         <Card
           key={index}
@@ -31,7 +42,7 @@ const Cards = () => {
             boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
             marginBottom: 50
           }}*/
-          className = "w-[300px] h-[400px] mb-[50px] shadow-md"
+          className="w-[300px] h-[400px] mb-[50px] shadow-md"
         >
           <p>{item.content}</p>
         </Card>
